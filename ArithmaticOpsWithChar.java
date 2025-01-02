87609e36c54d2019fa5a010a74d0ac81f92b55dc
LiyanshuBafna <lbafna@gitam.in>
2025-01-02 23:41:17 +0530

    Add files via upload

---
+++ ArithmaticOpsWithChar.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class ArithmaticOpsWithChar{
+    public static void main(String[] args){
+        char a='A';
+        char b='B';
+        System.out.println("SUM="+(a+b));
+        System.out.println("DIFFERENCE="+(a-b));
+        System.out.println("PRODUCT="+(a*b));
+        System.out.println("DIVISION="+(a/b));
+        System.out.println("REMAINDER="+(a%b));
+    }
+}
